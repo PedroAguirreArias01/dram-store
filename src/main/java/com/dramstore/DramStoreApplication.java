8831d3a849d63810650d71303c0af7f647e0c98b
Pedro Aguirre <pedroaguirrearias01@gmail.com>
2021-03-26 15:24:27 -0500

    commit init

---
+++ src/main/java/com/dramstore/DramStoreApplication.java
@@ -0,0 +1,13 @@
+package com.dramstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DramStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DramStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dramstore/DramStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dramstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DramStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
